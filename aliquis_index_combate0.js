5c04220cde4473dfdf200e614ced3665a46b9dc1
arthurAL_2022 <115472995+aaljim@users.noreply.github.com>
2022-11-20 12:21:09 -0300

    Update aliquis_index_combate0.js

--- aliquis_index_combate0.js
+++ aliquis_index_combate0.js
@@ -2055,7 +2055,7 @@ function applyPoint(next) {
     
 // SE OPT1 TEXT IGUAL A BATALHAR ENTÃO BASTA COLOCAR O LINK DA RESPECTIVA PÁGINA DE COMBATE
     if(point.opt1.text == "Batalhar"){
-        window.location = "battle_tests_batalha1.html";
+        window.location = "https://aaljim.github.io/Repositorio_jogo_I.N.C.E.L/battle_tests_batalha1.html";
     }
     if(point.opt1.text == "Você morreu"){
         window.location = "https://tiago070.github.io/integrador2022/morte_aliquis.html";

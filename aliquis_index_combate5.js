b2c04fcce7c297944d7c345e9ef9d6f5383a9a86
Gin <115474107+GinKatsumi@users.noreply.github.com>
2022-11-21 00:44:42 -0300

    Update aliquis_index_combate5.js

--- aliquis_index_combate5.js
+++ aliquis_index_combate5.js
@@ -2079,7 +2079,7 @@ setTimeout(() => {
         option1_bt.setAttribute("onclick", "window.location = 'https://aaljim.github.io/Repositorio_jogo_I.N.C.E.L/battle_tests_batalha5.html'");
     };
     if(point.opt1.text == "Você morreu"){
-        option1_bt.setAttribute("onclick", "window.location = 'https://tiago070.github.io/integrador2022/morte_aliquis.html'");
+        option1_bt.setAttribute("onclick", "window.location = 'morte_aliquis.html'");
     };
 
 

e4347bc0dc85fb05736cc39413510e307c94d6ed
Gin <115474107+GinKatsumi@users.noreply.github.com>
2022-11-21 00:33:17 -0300

    Update aliquis_index_combate2.js

--- aliquis_index_combate2.js
+++ aliquis_index_combate2.js
@@ -2053,11 +2053,8 @@ function applyPoint(next) {
 
     const point = history[next];
 
-    if(point.action == "Olá caro Jogador, antes de interagir com o século 18, que tal saber um pouco sobre minha história??"){
-        document.getElementById("scene-image").setAttribute("src", "https://i.imgur.com/UPFb4JC.jpeg")   
-    }
-    if(point.action == "Meu nome é Aliquis Petit, nasci em 1768, na região próxima ao Palácio de Versalhes. Não sei muito sobre meus pais, sou órfão desde que eu me entendo por gente. Minha maior ambição é conseguir fazer algo a respeito dessa monarquia, enquanto todos vivem na pobreza, o clero esnoba com suas festividades e negligenciam o próprio povo."){
-        document.getElementById("scene-image").setAttribute("src", "https://i.imgur.com/fpRVIT0.jpeg")   
+    if(point.action == "Você derrota mais um inimigo, e começa a ver que o treinamento não foi em vão."){
+        document.getElementById("scene-image").setAttribute("src", "https://i.imgur.com/8xHBe8h.jpg")   
     }
     
 // SE OPT1 TEXT IGUAL A BATALHAR ENTÃO BASTA COLOCAR O LINK DA RESPECTIVA PÁGINA DE COMBATE
